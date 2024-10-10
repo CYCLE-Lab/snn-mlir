f3571b48fda6f5960ffd7c0294dd8be64798ca8a
sheenisme <sheensong@163.com>
2024-10-10 20:13:16 +0800

    [pybind]fix pysnn errors temporary.

--- python/pysnn/snn_pybind.cpp
+++ python/pysnn/snn_pybind.cpp
@@ -2,17 +2,18 @@
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/Operation.h"
 #include "mlir/Bindings/Python/PybindAdaptors.h"
-#include "your_dialect.h" // 你自定义的Dialect和Ops定义头文件
+#include "SNN/SNNDialect.h"
+#include "SNN/SNNOps.h" // 你自定义的Dialect和Ops定义头文件
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(your_module_name, m) {
     // 绑定Dialect
-    mlir::python::exposeDialectInPython(m, mlir::your_namespace::YourDialect::getDialectNamespace(),
+    mlir::python::exposeDialectInPython(m, snn::getDialectNamespace(),
                                         [](mlir::MLIRContext *context) {
-        return std::make_unique<mlir::your_namespace::YourDialect>(context);
+        return std::make_unique<snn>(context);
     });
 
     // 绑定具体的Op
-    mlir::python::exposeOperationInPython<mlir::your_namespace::YourOp>(m, "YourOp");
+    mlir::python::exposeOperationInPython<snn::lifOp>(m, "lif");
 }
