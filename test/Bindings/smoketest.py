6308fb35dd0fd0f242cfaa360e427b4e099dbb3e
sheenisme <sheensong@163.com>
2024-10-16 16:35:07 +0800

    [test] fix binding test case.

--- test/Bindings/smoketest.py
+++ test/Bindings/smoketest.py
@@ -1,6 +1,5 @@
 # RUN: %python %s
 
-# export PYTHONPATH=$PYTHONPATH:/data/dagongcheng/sheensong-test/bigProject/snn-mlir/build/python_packages/snn_mlir_core
 from snn_mlir import *
 
-from mlir.dialects import *
+from mlir import *
