ead17aeb47beea9b8c8408d7c9f6c726b96ee21c
pengjh0111 <pengjh0111@163.com>
2024-10-16 17:25:05 +0800

    [comments] Update comments.

--- lib/Conversion/SNNToLinalgOps/SNNToLinalgOps.cpp
+++ lib/Conversion/SNNToLinalgOps/SNNToLinalgOps.cpp
@@ -29,127 +29,92 @@
 
 using namespace mlir;
 
-//定义computeIteratorTypesAndIndexingMaps函数
+// to define a computeIteratorTypesAndIndexingMaps func
 static std::tuple<SmallVector<utils::IteratorType>, SmallVector<AffineMap>>
 computeIteratorTypesAndIndexingMaps(OpBuilder &builder, int64_t inputRank) {
-    // 所有维度都并行处理
     SmallVector<utils::IteratorType> iteratorTypes(inputRank, utils::IteratorType::parallel);
-    
     MLIRContext *ctxt = builder.getContext();
-    // 创建单位映射，表示逐元素访问
+    // Create an identity mapping, representing element-wise access.
     auto identityMap = AffineMap::getMultiDimIdentityMap(inputRank, ctxt);
-    
-    // 只需要一个映射，表示输入输出的维度直接对应
     SmallVector<AffineMap> indexingMaps{identityMap, identityMap, identityMap};
-
     return std::make_tuple(iteratorTypes, indexingMaps);
 }
 
-// 定义 LIF 操作的转换模式
+// To define a conversion pattern for the LIF 
 struct lifOpConversion : public OpRewritePattern<snn::lifOp> {
   using OpRewritePattern::OpRewritePattern;
   LogicalResult matchAndRewrite(snn::lifOp op, PatternRewriter &rewriter) const override {
-  // 获取输入电压和输入信号
-  Value voltage = op.getVoltage();
-  Value input = op.getInputs();
+    Value voltage = op.getVoltage();
+    Value input = op.getInputs();
+    Location loc = op.getLoc();
+    float tau = 0.01f;
+    float threshold = 1.0f;
+    // calculate (voltage = voltage * (1 - tau) + input)
+    Type voltageType = voltage.getType();
+    float subtau = 1.0f - tau;
+    Value subtauValue = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32Type(), rewriter.getF32FloatAttr(subtau));
+    Value tauTensor = rewriter.create<tensor::EmptyOp>(loc, cast<ShapedType>(voltageType).getShape(), cast<ShapedType>(voltageType).getElementType());
+    Value tauBroadcast = rewriter.create<linalg::FillOp>(loc, subtauValue, tauTensor).getResult(0);
+    Value outVoltage = rewriter.create<tensor::EmptyOp>(loc, cast<ShapedType>(voltageType).getShape(), cast<ShapedType>(voltageType).getElementType());
+    Value decayedVoltage = rewriter.create<linalg::MulOp>(loc, outVoltage.getType(), ValueRange{voltage, tauBroadcast}, outVoltage).getResult(0);
+    Value finalVoltage = rewriter.create<tensor::EmptyOp>(loc, cast<ShapedType>(voltageType).getShape(),cast<ShapedType>(voltageType).getElementType());
+    Value addtensor = rewriter.create<linalg::AddOp>(loc, finalVoltage.getType(), ValueRange{input, decayedVoltage}, finalVoltage ).getResult(0);
+    Value thresholdemp = rewriter.create<tensor::EmptyOp>(loc, cast<ShapedType>(addtensor.getType()).getShape(), cast<ShapedType>(addtensor.getType()).getElementType());
+    Value thresholdValue = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32Type(), rewriter.getF32FloatAttr(threshold));
+    Value thresholdTensor = rewriter.create<linalg::FillOp>(loc, thresholdValue, thresholdemp).getResult(0);
 
-  // 常量定义
-  Location loc = op.getLoc();
-  float tau = 0.01f;
-  float threshold = 1.0f;
-  // 计算衰减电压和输入加权和 (voltage = voltage * (1 - tau) + input)
-  //计算(1 - tau) 并广播成一个tensor
-  Type voltageType = voltage.getType();
-  float subtau = 1.0f - tau;
-  //auto subtauAttr = rewriter.getF32FloatAttr(subtau);
-  Value subtauValue = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32Type(), rewriter.getF32FloatAttr(subtau));
-  Value tauTensor = rewriter.create<tensor::EmptyOp>(loc, cast<ShapedType>(voltageType).getShape(), cast<ShapedType>(voltageType).getElementType());
-  Value tauBroadcast = rewriter.create<linalg::FillOp>(loc, subtauValue, tauTensor).getResult(0);
+    auto inputRank = cast<ShapedType>(voltageType).getRank();
+    auto [iteratorTypes, indexingMaps] = computeIteratorTypesAndIndexingMaps(rewriter, inputRank);
+    auto cmpType = RankedTensorType::get(dyn_cast<RankedTensorType>(voltageType).getShape(), dyn_cast<RankedTensorType>(voltageType).getElementType()); 
+    Value cmpTensoremp = rewriter.create<tensor::EmptyOp>(loc, dyn_cast<RankedTensorType>(voltageType).getShape(), dyn_cast<RankedTensorType>(voltageType).getElementType());
 
-  //计算voltage * (1 - tau)
-  Value outVoltage = rewriter.create<tensor::EmptyOp>(loc, cast<ShapedType>(voltageType).getShape(), cast<ShapedType>(voltageType).getElementType());
-  Value decayedVoltage = rewriter.create<linalg::MulOp>(loc, outVoltage.getType(), ValueRange{voltage, tauBroadcast}, outVoltage).getResult(0);
-
-  //计算voltage * (1 - tau) + input
-  Value finalVoltage = rewriter.create<tensor::EmptyOp>(loc, cast<ShapedType>(voltageType).getShape(),cast<ShapedType>(voltageType).getElementType());
-  Value addtensor = rewriter.create<linalg::AddOp>(loc, finalVoltage.getType(), ValueRange{input, decayedVoltage}, finalVoltage ).getResult(0);
-
-  //构建阈值tensor
-  Value thresholdemp = rewriter.create<tensor::EmptyOp>(loc, cast<ShapedType>(addtensor.getType()).getShape(), cast<ShapedType>(addtensor.getType()).getElementType());
-  Value thresholdValue = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32Type(), rewriter.getF32FloatAttr(threshold));
-  Value thresholdTensor = rewriter.create<linalg::FillOp>(loc, thresholdValue, thresholdemp).getResult(0);
-
-  // 获取输入张量的类型
-  auto inputRank = cast<ShapedType>(voltageType).getRank(); // 输入的维度
-  // auto dim = 0; 
-
-  // 使用 computeIteratorTypesAndIndexingMaps 创建迭代器类型和索引映射
-  auto [iteratorTypes, indexingMaps] = computeIteratorTypesAndIndexingMaps(rewriter, inputRank);
-
-  // 创建输出张量
-  auto cmpType = RankedTensorType::get(dyn_cast<RankedTensorType>(voltageType).getShape(), dyn_cast<RankedTensorType>(voltageType).getElementType()); 
-  Value cmpTensoremp = rewriter.create<tensor::EmptyOp>(loc, dyn_cast<RankedTensorType>(voltageType).getShape(), dyn_cast<RankedTensorType>(voltageType).getElementType());
-
-  // 创建 linalg::GenericOp
-  auto updataop = rewriter.create<linalg::GenericOp>(
-      loc, cmpType, // 输出类型
-      ValueRange{thresholdTensor, addtensor}, // 输入张量
-      cmpTensoremp, // 输出张量
-      indexingMaps, // 使用生成的索引映射
-      iteratorTypes, // 使用生成的迭代器类型
-      [&](OpBuilder &b, Location loc, ValueRange args) {
-          Value thresholdElem = args[0]; // 获取阈值元素
-          Value voltageElem = args[1]; // 获取电压元素
-          // 进行比较操作，生成布尔值
-          Value comparisonResult = b.create<arith::CmpFOp>(loc, arith::CmpFPredicate::OLT, voltageElem, thresholdElem); // voltageElem小于thresholdElem 则返回true
-          // 根据比较结果生成浮点类型的0或1
-          Value floatZero = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(0.0f));
-          Value floatOne = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(1.0f));
-          // 选择性地返回0或1
-          Value result = b.create<arith::SelectOp>(loc, comparisonResult, floatZero, floatOne);
-          b.create<linalg::YieldOp>(loc, result); // 返回结果
-      }
-  );
-  Value updataTensor = updataop.getResult(0); // 获取更新后的张量
-  // 用新的 voltage 替换原始的 LIF 操作
-  rewriter.replaceOp(op, updataTensor);
+    // build linalg::GenericOp
+    auto updataop = rewriter.create<linalg::GenericOp>(
+        loc, cmpType, 
+        ValueRange{thresholdTensor, addtensor}, 
+        cmpTensoremp, 
+        indexingMaps, 
+        iteratorTypes, 
+        [&](OpBuilder &b, Location loc, ValueRange args) {
+            Value thresholdElem = args[0]; 
+            Value voltageElem = args[1]; 
+            Value comparisonResult = b.create<arith::CmpFOp>(loc, arith::CmpFPredicate::OLT, voltageElem, thresholdElem);
+            Value floatZero = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(0.0f));
+            Value floatOne = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(1.0f));
+            Value result = b.create<arith::SelectOp>(loc, comparisonResult, floatZero, floatOne);
+            b.create<linalg::YieldOp>(loc, result); 
+        }
+    );
+    Value updataTensor = updataop.getResult(0); 
+    rewriter.replaceOp(op, updataTensor);
+    
     return success();
   }
 };
 
 namespace{
+  class SNNToLinalgOpsPass : public mlir::PassWrapper<SNNToLinalgOpsPass, OperationPass<ModuleOp>> {
+  public:
+    MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(SNNToLinalgOpsPass)
 
-class SNNToLinalgOpsPass : public mlir::PassWrapper<SNNToLinalgOpsPass, OperationPass<ModuleOp>> {
-public:
-  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(SNNToLinalgOpsPass)
+    void getDependentDialects(mlir::DialectRegistry &registry) const override {
+    registry.insert<mlir::arith::ArithDialect, 
+                    mlir::scf::SCFDialect,
+                    mlir::linalg::LinalgDialect, 
+                    mlir::tensor::TensorDialect>();
+    }
 
+    StringRef getArgument() const final {
+      return "convert-snn-to-linalg";  
+    }
 
-  void getDependentDialects(mlir::DialectRegistry &registry) const override {
-  registry.insert<mlir::arith::ArithDialect, 
-                  mlir::scf::SCFDialect,
-                  mlir::linalg::LinalgDialect, 
-                  mlir::tensor::TensorDialect>();  // 添加 TensorDialect
-}
+    StringRef getDescription() const final {
+      return "Convert SNN operations to Linalg operations."; 
+    }
 
-
-
-  StringRef getArgument() const final {
-    return "convert-snn-to-linalg";  // 命令行标识符
-  }
-
-  StringRef getDescription() const final {
-    return "Convert SNN operations to Linalg operations.";  // 描述
-  }
-
-  // void getDependentDialects(mlir::DialectRegistry &registry) const override {
-  //   registry.insert<mlir::arith::ArithDialect, mlir::scf::SCFDialect>();
-  // }
-
-  void runOnOperation() final;
-};
-}
-
-
+    void runOnOperation() final;
+  };
+}//namespace
 
 void SNNToLinalgOpsPass::runOnOperation() {
   mlir::ConversionTarget target(getContext());
@@ -159,12 +124,10 @@ void SNNToLinalgOpsPass::runOnOperation() {
                          mlir::scf::SCFDialect,
                          mlir::linalg::LinalgDialect,
                          mlir::tensor::TensorDialect>();
-  target.addLegalOp<mlir::linalg::FillOp>(); // 添加 FillOp 到合法操作中
+  target.addLegalOp<mlir::linalg::FillOp>();
 
-  // 创建转换模式集
   mlir::RewritePatternSet patterns(&getContext());
   patterns.add<lifOpConversion>(&getContext());
-
   if (mlir::failed(mlir::applyPartialConversion(getOperation(), target,
                                                 std::move(patterns)))) {
     signalPassFailure();
@@ -175,7 +138,6 @@ std::unique_ptr<mlir::Pass> snn::createSNNToLinalgOpsPass() {
   return std::make_unique<SNNToLinalgOpsPass>();
 }
 
-// 注册 Pass
 static mlir::PassRegistration<SNNToLinalgOpsPass> pass;
 
 
--- lib/Conversion/SNNToStd/SNNToStd.cpp
+++ lib/Conversion/SNNToStd/SNNToStd.cpp
@@ -28,147 +28,95 @@
 
 using namespace mlir;
 
-
-// 定义 LIF 操作的转换模式
+// To define a conversion pattern for the LIF
 struct lifOpLowering : public OpRewritePattern<snn::lifOp> {
   using OpRewritePattern::OpRewritePattern;
-
   LogicalResult matchAndRewrite(snn::lifOp op, PatternRewriter &rewriter) const override {
-  // 获取输入电压和输入信号
-  Value voltage = op.getVoltage();
-  Value input = op.getInputs();
+    Value voltage = op.getVoltage();
+    Value input = op.getInputs();
+    Location loc = op.getLoc();
+    float tau = 0.01f;
+    float threshold = 1.0f;
 
-  // 常量定义
-  Location loc = op.getLoc();
-  float tau = 0.01f;
-  float threshold = 1.0f;
+    Value tauConst = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(tau));
+    auto tauTensor = rewriter.create<tensor::SplatOp>(loc, voltage.getType(), tauConst);
+    Value oneMinusTau = rewriter.create<arith::SubFOp>(loc, rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(1.0f)), tauTensor);
+    Value oneMinusTauTensor = rewriter.create<tensor::SplatOp>(loc, voltage.getType(), oneMinusTau);
+    Value decayedVoltage = rewriter.create<arith::MulFOp>(loc, voltage, oneMinusTauTensor);
+    Value oneConst = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(1.0f));
+    Value oneTensor = rewriter.create<tensor::SplatOp>(loc, input.getType(), oneConst);
+    Value inputTerm = rewriter.create<arith::MulFOp>(loc, input, oneTensor);
+    Value updatedVoltage = rewriter.create<arith::AddFOp>(loc, decayedVoltage, inputTerm);
+    Value thresholdVal = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(threshold));
+    Value thresholdTensor = rewriter.create<tensor::SplatOp>(loc, updatedVoltage.getType(), thresholdVal);
+    auto tType = cast<RankedTensorType>(updatedVoltage.getType());
+    auto shape = tType.getShape();
+    Value cmp = rewriter.create<arith::CmpFOp>(loc, arith::CmpFPredicate::OGT, updatedVoltage, thresholdTensor);
 
-  // 1. 将标量 tau 转换为张量，以匹配 voltage 的类型
-  Value tauConst = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(tau));
-  auto tauTensor = rewriter.create<tensor::SplatOp>(loc, voltage.getType(), tauConst);
-
-  // 计算衰减电压和输入加权和 (voltage = voltage * (1 - tau) + input)
-  Value oneMinusTau = rewriter.create<arith::SubFOp>(loc, rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(1.0f)), tauTensor);
-  Value oneMinusTauTensor = rewriter.create<tensor::SplatOp>(loc, voltage.getType(), oneMinusTau);
-
-  // 计算 decayedVoltage
-  Value decayedVoltage = rewriter.create<arith::MulFOp>(loc, voltage, oneMinusTauTensor);
-
-  // 将标量 1.0f 转换为与 input 相同类型的张量
-  Value oneConst = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(1.0f));
-  Value oneTensor = rewriter.create<tensor::SplatOp>(loc, input.getType(), oneConst);
-
-  // 使用张量进行乘法
-  Value inputTerm = rewriter.create<arith::MulFOp>(loc, input, oneTensor);
-
-  // 计算更新后的电压
-  Value updatedVoltage = rewriter.create<arith::AddFOp>(loc, decayedVoltage, inputTerm);
-
-  // 2. 检查是否超过阈值 (if updatedVoltage > threshold)
-  Value thresholdVal = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(threshold));
-  Value thresholdTensor = rewriter.create<tensor::SplatOp>(loc, updatedVoltage.getType(), thresholdVal);
-
-    // 获取张量的形状
-  auto tType = cast<RankedTensorType>(updatedVoltage.getType());
-  auto shape = tType.getShape();
-  // auto emptyTensor = rewriter.create<tensor::EmptyOp>(loc, shape, rewriter.getF32Type());
-  // llvm::outs() << emptyTensor;
-
-  // 对张量进行比较
-  Value cmp = rewriter.create<arith::CmpFOp>(loc, arith::CmpFPredicate::OGT, updatedVoltage, thresholdTensor);
-
-  // 创建scf::ForOp来遍历每个元素
-  Value zeroIndex = rewriter.create<arith::ConstantIndexOp>(loc, 0);
-  Value dimX = rewriter.create<arith::ConstantIndexOp>(loc, shape[0]);
-  Value dimY = rewriter.create<arith::ConstantIndexOp>(loc, shape[1]);
-
-  Value OneConsValue = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(1.0f));
-  Value ZeroConsValue = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(0.0f));
-
-
-  Value oneIndex = rewriter.create<arith::ConstantIndexOp>(loc, 1);
-
-  auto outerForOp = rewriter.create<scf::ForOp>(
-    loc, zeroIndex, dimX, oneIndex, updatedVoltage,
-    [&](OpBuilder &builder, Location loc, Value i, ValueRange iterArgs) {
-      Value currentTensor = iterArgs[0];
-      auto innerForOp = builder.create<scf::ForOp>(
-        loc, zeroIndex, dimY, oneIndex, currentTensor,
-        [&](OpBuilder &b, Location loc, Value j, ValueRange innerIterArgs) {
-          Value elemCmp = b.create<tensor::ExtractOp>(loc, cmp, ValueRange{i, j});
-          auto ifOp = b.create<scf::IfOp>(loc, elemCmp, 
-            [&](OpBuilder &b2, Location loc) {  
-              Value newTensor = b2.create<tensor::InsertOp>(loc, OneConsValue, innerIterArgs[0], ValueRange{i, j});
-              b2.create<scf::YieldOp>(loc,newTensor);
-            },
-            [&](OpBuilder &b2, Location loc) {  // else 分支
-              Value newTensor = b2.create<tensor::InsertOp>(loc, ZeroConsValue, innerIterArgs[0], ValueRange{i, j});
-              b2.create<scf::YieldOp>(loc,newTensor);
-            });
-          b.create<scf::YieldOp>(loc, ifOp->getResult(0));             
-        });      
-      builder.create<scf::YieldOp>(loc, innerForOp->getResult(0));
-    });
-
-  auto newTensor = outerForOp->getResult(0);
-  // 3. 如果超过阈值，生成脉冲并重置电压
-  // rewriter.create<scf::IfOp>(loc, trueConstant, [&](OpBuilder &b, Location loc) {
-  //   b.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(0.0f));
-  //   b.create<scf::YieldOp>(loc);
-  // });
-
-  // 用新的 voltage 替换原始的 LIF 操作
-  rewriter.replaceOp(op, newTensor);
-
-  return success();
-}
-
-
+    // build scf::ForOp
+    Value zeroIndex = rewriter.create<arith::ConstantIndexOp>(loc, 0);
+    Value dimX = rewriter.create<arith::ConstantIndexOp>(loc, shape[0]);
+    Value dimY = rewriter.create<arith::ConstantIndexOp>(loc, shape[1]);
+    Value OneConsValue = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(1.0f));
+    Value ZeroConsValue = rewriter.create<arith::ConstantOp>(loc, rewriter.getF32FloatAttr(0.0f));
+    Value oneIndex = rewriter.create<arith::ConstantIndexOp>(loc, 1);
+    auto outerForOp = rewriter.create<scf::ForOp>(
+      loc, zeroIndex, dimX, oneIndex, updatedVoltage,
+      [&](OpBuilder &builder, Location loc, Value i, ValueRange iterArgs) {
+        Value currentTensor = iterArgs[0];
+        auto innerForOp = builder.create<scf::ForOp>(
+          loc, zeroIndex, dimY, oneIndex, currentTensor,
+          [&](OpBuilder &b, Location loc, Value j, ValueRange innerIterArgs) {
+            Value elemCmp = b.create<tensor::ExtractOp>(loc, cmp, ValueRange{i, j});
+            auto ifOp = b.create<scf::IfOp>(loc, elemCmp, 
+              [&](OpBuilder &b2, Location loc) {  
+                Value newTensor = b2.create<tensor::InsertOp>(loc, OneConsValue, innerIterArgs[0], ValueRange{i, j});
+                b2.create<scf::YieldOp>(loc,newTensor);
+              },
+              [&](OpBuilder &b2, Location loc) {
+                Value newTensor = b2.create<tensor::InsertOp>(loc, ZeroConsValue, innerIterArgs[0], ValueRange{i, j});
+                b2.create<scf::YieldOp>(loc,newTensor);
+              });
+            b.create<scf::YieldOp>(loc, ifOp->getResult(0));             
+          });      
+        builder.create<scf::YieldOp>(loc, innerForOp->getResult(0));
+      });
+    auto newTensor = outerForOp->getResult(0);
+    rewriter.replaceOp(op, newTensor);
+    return success();
+  }
 };
 
 namespace{
-// class SNNToStdPass
-//     : public mlir::PassWrapper<SNNToStdPass,
-//                                mlir::OperationPass<mlir::ModuleOp>> {
-class SNNToStdPass : public mlir::PassWrapper<SNNToStdPass, OperationPass<ModuleOp>> {
-public:
-  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(SNNToStdPass)
+  class SNNToStdPass : public mlir::PassWrapper<SNNToStdPass, OperationPass<ModuleOp>> {
+  public:
+    MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(SNNToStdPass)
 
+    void getDependentDialects(mlir::DialectRegistry &registry) const override {
+      registry.insert<mlir::arith::ArithDialect, 
+                      mlir::scf::SCFDialect, 
+                      mlir::tensor::TensorDialect>();
+    }
 
-  void getDependentDialects(mlir::DialectRegistry &registry) const override {
-  registry.insert<mlir::arith::ArithDialect, 
-                  mlir::scf::SCFDialect, 
-                  mlir::tensor::TensorDialect>();  // 添加 TensorDialect
-}
+    StringRef getArgument() const final {
+      return "convert-snn-to-std";
+    }
 
+    StringRef getDescription() const final {
+      return "Convert SNN operations to standard operations.";
+    }
 
-
-  StringRef getArgument() const final {
-    return "convert-snn-to-std";  // 命令行标识符
-  }
-
-  StringRef getDescription() const final {
-    return "Convert SNN operations to standard operations.";  // 描述
-  }
-
-  // void getDependentDialects(mlir::DialectRegistry &registry) const override {
-  //   registry.insert<mlir::arith::ArithDialect, mlir::scf::SCFDialect>();
-  // }
-
-  void runOnOperation() final;
-};
-}
-
-
+    void runOnOperation() final;
+  };
+}//namespace
 
 void SNNToStdPass::runOnOperation() {
   mlir::ConversionTarget target(getContext());
-
+  
   target.addIllegalDialect<snn::SNNDialect>();
   target.addLegalDialect<mlir::arith::ArithDialect,
                          mlir::scf::SCFDialect,
                          mlir::tensor::TensorDialect>();
-
   mlir::RewritePatternSet patterns(&getContext());
   patterns.add<lifOpLowering>(&getContext());
 
@@ -182,7 +130,6 @@ std::unique_ptr<mlir::Pass> snn::createSNNToStdPass() {
   return std::make_unique<SNNToStdPass>();
 }
 
-// 注册 Pass
 static mlir::PassRegistration<SNNToStdPass> pass;
 
 
